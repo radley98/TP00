b1ceb54ef8b1a6e0a460d0ebe00ecd7118d82478
radley <radleybao@gmail.com>
2024-04-05 11:26:55 +0800

    Initial Commit

---
+++ TP00/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TP00
+//
+//  Created by Rad on 2024/4/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TP00/TP00App.swift
@@ -0,0 +1,17 @@
+//
+//  TP00App.swift
+//  TP00
+//
+//  Created by Rad on 2024/4/5.
+//
+
+import SwiftUI
+
+@main
+struct TP00App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
